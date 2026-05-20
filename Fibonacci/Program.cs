c923f3b5658775dcaf405cf2b0aa3b8aa08f4c3f
jos3ric4rd0 <lol.ji@hotmail.com>
2026-05-20 10:50:56 -0300

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static List<int> Fibonacci(int x)
+    {
+        List<int> resultado = new List<int>();
+
+        if (x <= 0) return resultado;
+
+        if (x >=1) resultado.Add(0);
+
+        if (x >=2) resultado.Add(1);
+
+        for (int i = 2; i < x; i++)
+        {
+            int proximo = resultado[i - 1] + resultado[i - 2];
+            resultado.Add(proximo);
+        }
+
+        return resultado;
+    }
+
+    // Testes
+    /*
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", Fibonacci(3))); // 0, 1, 1
+        Console.WriteLine(string.Join(", ", Fibonacci(5))); // 0, 1, 1, 2, 3
+        Console.WriteLine(string.Join(", ", Fibonacci(7))); // 0, 1, 1, 2, 3, 5, 8
+    }
+    */
+}
